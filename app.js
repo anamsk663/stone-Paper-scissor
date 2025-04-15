23394908964adb6fbbefd5cf37af1117cd846ae6
anamsk663 <167622114+anamsk663@users.noreply.github.com>
2025-04-15 18:42:12 +0530

    errors in Js

--- app.js
+++ app.js
@@ -2,10 +2,59 @@ let userScore = 0;
 let compScore = 0;
 
 const choices = document.querySelectorAll(".choice");
+const msg = document.querySelector("#msg");
 
-choices.forEach((choices) =>{
-        console.log(choice); 
-        choice.addEventListner("click", () =>{
-            console.log("choice was clicked");
-        })
-})
+const genCompChoice = () =>{
+    const options = ["rock","paper","scissor"];
+    const randIdx = Math.floor(Math.random() * 3);
+    return options[randIdx];
+};
+
+const drawGame = () => {
+    console.log("game was draw");
+    msg.innerText = "Game was draw. Play again";
+};
+
+const showWinner = (userwin, userChoice, compChoice) => {
+    if(userwin) {
+        console.log("you win!");
+        msg.innerText = `You win! ${userChoice} beats ${compChoice}`;
+        msg.style.backgroundColor = "green";
+    }else {
+        console.log("you lose");
+        msg.innerText = `You lose. ${compChoice} beats ${userChoice}`;
+        msg.style.backgroundColor = "red";
+    }
+};
+
+const playGame = (userChoice) =>{
+    console.log("user choice =" , userChoice);
+    //generate computer choice
+    const compChoice = genCompChoice();
+    console.log ("comp choice =", compChoice);
+
+    if(userChoice === compChoice){
+        //draw game
+        drawGame();
+    }
+    else{
+        let userwin = true;
+        if (userChoice === "rock"){
+            userwin =  compChoice === "paper" ? false : true;   
+        } else if(userChoice === "paper")   {
+            userwin =  compChoice === "scissors" ? false : true;
+        } else {
+            userwin = compChoice === "rock" ? false : true;
+        }
+        showWinner(userwin, userChoice, compChoice);
+    }
+
+};
+
+choices.forEach((choice) => {
+        choice.addEventListner("click", () => {
+        const userChoice = choice.getAttribute("Id");
+        console.log("choice was clicked", userChoice);
+        playGame(userChoice);
+        });
+}); 
