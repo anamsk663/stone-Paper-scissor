47b6a982ae201ef2ad05034f7f4ff97edc73b068
anamsk663 <167622114+anamsk663@users.noreply.github.com>
2025-04-15 17:34:58 +0530

    Project initailized

---
+++ app.js
@@ -0,0 +1,11 @@
+let userScore = 0;
+let compScore = 0;
+
+const choices = document.querySelectorAll(".choice");
+
+choices.forEach((choices) =>{
+        console.log(choice); 
+        choice.addEventListner("click", () =>{
+            console.log("choice was clicked");
+        })
+})
